23034f46252b99a13863a14a0b1237cdb9bb9959
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2026-09-25 17:32:07 +0700

    feat: testing migration manual

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -7,5 +7,9 @@ export const users = pgTable('users', {
   password: text().notNull(),
   avatar: text().notNull(),
   createdAt: timestamp().notNull().defaultNow(),
-  phone: text()
+  phone: text() //tetap disarankan mengubah tipe data lewat schema langsung karena nanti akan dicatet langsung migrations perubahaannya
+  // create migrations :npx nuxt db generate --custom --name drop_phone atau 
+  //npx drizzle-kit generate --config=./.nuxt/hub/db/drizzle.config.ts --custom --name=drop_phone
+  // npx nuxt db migrate
+  // npx nuxt db generate
 })
