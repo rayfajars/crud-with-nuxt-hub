efb148338a6765843135f559c7d6dc02dbc196ed
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2026-09-25 16:59:45 +0700

    first: commit

---
+++ server/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, text, serial, timestamp } from 'drizzle-orm/pg-core'
+
+export const users = pgTable('users', {
+  id: serial().primaryKey(),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  password: text().notNull(),
+  avatar: text().notNull(),
+  createdAt: timestamp().notNull().defaultNow(),
+  phone: text()
+})
