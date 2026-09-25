b6859c68dd0dd48791314bdb8e81bd10564c6596
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2026-09-25 18:05:51 +0700

    add table and migrateions users

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -4,12 +4,7 @@ export const users = pgTable('users', {
   id: serial().primaryKey(),
   name: text().notNull(),
   email: text().notNull().unique(),
-  password: text().notNull(),
+  phone: text(),
   avatar: text().notNull(),
   createdAt: timestamp().notNull().defaultNow(),
-  phone: text() //tetap disarankan mengubah tipe data lewat schema langsung karena nanti akan dicatet langsung migrations perubahaannya
-  // create migrations :npx nuxt db generate --custom --name drop_phone atau 
-  //npx drizzle-kit generate --config=./.nuxt/hub/db/drizzle.config.ts --custom --name=drop_phone
-  // npx nuxt db migrate
-  // npx nuxt db generate
 })
